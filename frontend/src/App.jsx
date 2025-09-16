74f2a6381e4cedc914446c89ec91601a214ffa0e
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2025-09-16 16:49:39 -0400

    implementação de biblioteca mui para a criação de icones e a criação de cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Cards from './componnents/Cards'
 
 function App() {
   const [,] = useState(0)
 
   return (
     <>
-      
+      <Cards />
     </>
   )
 }
