0c22a37e0588c887690fbc4514f948b9b6fa3202
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2025-09-22 21:45:09 -0400

    Deploy
    Realização do Deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
